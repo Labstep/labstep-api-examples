e7ac2fc84c8da2483b4959f619d3605d98b9a3ac
Jan Domanski <jandom@gmail.com>
2018-04-11 10:11:30 +0100

    Add all the example scripts

---
+++ create-post.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import json
+import requests
+
+headers = {
+    'apikey': 'YOUR-API-KEY',
+}
+
+data = {
+    'name': 'Labstep Post API',
+    'description': 'Create post with custom description on your timeline',
+}
+print(data)
+url = 'https://api.labstep.com/api/generic/post'
+r = requests.post(
+    url,
+    json=data,
+    headers=headers,
+)
+
+parsed = json.loads(r.content)
+print(json.dumps(parsed, indent=4, sort_keys=True))
